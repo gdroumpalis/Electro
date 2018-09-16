5c189dbae5d98148476e6c230bdafc0b3a6c1d7f
gdroumpalis <georgiosdroubalis@hotmail.gr>
2018-09-16 21:52:38 +0300

    threading test for renderer

--- Renderer/MRenderer.py
+++ Renderer/MRenderer.py
@@ -1,60 +1,40 @@
+from PyQt5.QtCore import QThread
 from numpy import *
 from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 import serial
 import datetime
-# Create object serial port
-portName = "/dev/ttyUSB0"                      # replace this port name by yours!
-baudrate = 1200
-ser = serial.Serial(portName,baudrate)
+import Sources.RendererUtilities as rutils
 
-### START QtApp #####
-app = QtGui.QApplication([])            # you MUST do this once (initialize things)
-####################
 
-win = pg.GraphicsWindow(title="Signal from serial port") # creates a window
-p = win.addPlot(title="Temp plot")  # creates empty space for the plot in the window
-p2 = win.addPlot(title="Avg Temp Plot")
-curve = p.plot()                        # create an empty "plot" (a curve to plot)
-curve2 = p2.plot()
-windowWidth = 500                       # width of the window displaying the curve
-Xm = linspace(0,0,windowWidth)          # create array that will contain the relevant time series
-Am = linspace(0,0,windowWidth)
-ptr = 1                      # set first x position
+class RendererThread(QThread):
 
-# Realtime data plot. Each time this function is called, the data display is updated
-def update(f):
-    global curve,curve2, ptr, Xm , Am
-    Xm[:-1] = Xm[1:]                      # shift data in the temporal mean 1 sample left
-    Am[:-1] = Am[1:]
-    value = ser.readline()                # read line (single value) from the serial port
-    Xm[-1] = float(value)                 # vector containing the instantaneous values
-    if ptr<=500:
-        Am[-1] = sum(Xm)/ptr
-    else:
-        Am[-1] = sum(Xm)/len(Xm)
-    print("current temp:{} , avg temp{}".format(Xm[-1],Am[-1]))
-    f.write("datetime:{} => current temp:{} , avg temp{}\n".format(datetime.datetime.now(),Xm[-1],Am[-1]))
-    ptr += 1                              # update x position for displaying the curve
-    curve.setData(Xm)                     # set the curve with this data
-    curve.setPos(ptr,1)                   # set x position in the graph to 0
-    curve2.setData(Am)
-    curve2.setPos(ptr,1)
-    QtGui.QApplication.processEvents()    # you MUST process the plot now
+    def __init__(self, rendererparams=rutils.RendererParameters(False, False, 600, 600), cancel=False):
+        QThread.__init__(self)
+        # self.params = rendererparams
+        # self.win = pg.GraphicsWindow(title="Signal from serial port")  # creates a window
+        # self.p = self.win.addPlot(title="Temp plot")  # creates empty space for the plot in the window
+        # self.p2 = self.win.addPlot(title="Avg Temp Plot")
+        # self.curve = self.p.plot()  # create an empty "plot" (a curve to plot)
+        # self.curve2 = self.p2.plot()
+        # self.windowWidth = 500  # width of the window displaying the curve
+        # self.Xm = linspace(0, 0, rendererparams.renderingwindowheigh)  # create array that will contain the relevant time series
+        # self.Am = linspace(0, 0, rendererparams.renderingwindowwidth)
+        # self.ptr = 1
+        self.cancel = cancel
 
-### MAIN PROGRAM #####
-# this is a brutal infinite loop calling your realtime data plot
-try:
-    f = open("log.txt","w+")
-    while True:
-        update(f)
-except KeyboardInterrupt:
-    print("finished")
-finally:
-    ser.close()
-    f.close()
-    pg.QtGui.QApplication.exec_()
+    def run(self):
+        try:
+            # f = open("log.txt", "w+")
+            while not self.cancel:
+                self.update()
+        finally:
+            self.finished()
 
-### END QtApp ####
-#pg.QtGui.QApplication.exec_() # you MUST put this at the end
-##################
+
+    def update(self):
+        print("testing")
+
+
+    def finished(self):
+        print("finished")
--- Sources/mainui.py
+++ Sources/mainui.py
@@ -1,8 +1,8 @@
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 import UI.mainui
 import Utilities.EventConnector as EVC
-
-
+import Sources.RendererUtilities as rutils
+import Renderer.MRenderer as mrenderer
 class MainUI(QMainWindow):
 
     def __init__(self):
@@ -12,11 +12,23 @@ class MainUI(QMainWindow):
         self.connectuicomponetstosignal()
         self.attachkeyboardshortcuts()
 
+
     def connectuicomponetstosignal(self):
         EVC.connect(self.ui.actionClose.triggered, self.closeapplication)
+        EVC.connect(self.ui.startpushbutton.clicked , self.startthread)
+        EVC.connect(self.ui.stoppushbutton.clicked,self.stopmethod)
 
     def attachkeyboardshortcuts(self):
         self.ui.actionClose.setShortcut("ctrl+q")
 
     def closeapplication(self):
         self.close()
+
+    def startthread(self):
+        rthreadparams = rutils.RendererParameters(False, False, 600, 600,"")
+        self.cancel = False
+        self.rthread = mrenderer.RendererThread(rthreadparams , self.cancel)
+        self.rthread.start()
+
+    def stopmethod(self):
+        self.rthread.cancel = True
