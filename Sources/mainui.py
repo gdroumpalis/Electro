e5ecfd1be51880f5c02aa7a509029757a6657967
gdroumpalis <georgiosdroubalis@hotmail.gr>
2018-09-16 13:03:03 +0300

    Create Initial project structure and added basic UI

---
+++ Sources/mainui.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+import UI.mainui
+import Utilities.EventConnector as EVC
+
+class MainUI(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.ui = UI.mainui.Ui_MainWindow()
+        self.ui.setupUi(self)
+        EVC.connect(self.ui.actionClose.triggered,self.CloseApplication)
+        self.ui.actionClose.setShortcut("ctrl+q")
+
+    def CloseApplication(self):
+        self.close()
