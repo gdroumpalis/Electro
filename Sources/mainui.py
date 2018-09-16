6d5dd204c12e996ad41f4b3abef1a262a883e85c
gdroumpalis <georgiosdroubalis@hotmail.gr>
2018-09-16 19:59:01 +0300

    refactoring main ui a little

--- Sources/mainui.py
+++ Sources/mainui.py
@@ -2,17 +2,21 @@ from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 import UI.mainui
 import Utilities.EventConnector as EVC
 
+
 class MainUI(QMainWindow):
 
     def __init__(self):
         super().__init__()
         self.ui = UI.mainui.Ui_MainWindow()
         self.ui.setupUi(self)
-        self.InitializeFileMenu()
+        self.connectuicomponetstosignal()
+        self.attachkeyboardshortcuts()
 
-    def InitializeFileMenu(self):
-        EVC.connect(self.ui.actionClose.triggered, self.CloseApplication)
+    def connectuicomponetstosignal(self):
+        EVC.connect(self.ui.actionClose.triggered, self.closeapplication)
+
+    def attachkeyboardshortcuts(self):
         self.ui.actionClose.setShortcut("ctrl+q")
 
-    def CloseApplication(self):
+    def closeapplication(self):
         self.close()
